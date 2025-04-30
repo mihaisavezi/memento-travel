d969cdd1252b3eea71fd94dd0bf92bccfcb05bf2
Mihai <mihai.ppscu@gmail.com>
2025-04-30 15:29:37 +0300

    feat: add landing page (#52)

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Easily document your travels and adventures with this intuitive travel log app. Save locations,
+          upload photos, and write notes to build a vivid digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github <Icon name="tabler:brand-github" />
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
