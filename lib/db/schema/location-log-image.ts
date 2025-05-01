88d634bc7316ae212f54dc895471637932be2bfe
Mihai <mihai.ppscu@gmail.com>
2025-05-01 09:42:54 +0300

    feat: setup db (#55)

---
+++ lib/db/schema/location-log-image.ts
@@ -0,0 +1,17 @@
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { locationLog } from "./location-log";
+
+export const locationLogImage = sqliteTable("locationLogImage", {
+  id: int().primaryKey({ autoIncrement: true }),
+  key: text().notNull(),
+  slug: text().notNull().unique(),
+  description: text(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  startedAt: int().notNull().$default(() => Date.now()),
+  endedAt: int().notNull().$default(() => Date.now()),
+  locationLogId: int().notNull().references(() => locationLog.id),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
