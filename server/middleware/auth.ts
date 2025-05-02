333cfb4cf8aeef654c5a5125a2b83e8e50e0e7e5
Mihai <mihai.ppscu@gmail.com>
2025-05-02 13:51:59 +0300

    Mt 22 (#62)
    * feat: create new locations
    
    * feat: add csrf token handling
    
    * fix: remove key for pnpm

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,11 +1,15 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-  if (event.path.startsWith("/dashboard")) {
-    const session = await auth.api.getSession({
-      headers: event.headers,
-    });
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
 
+  event.context.user = session && session.user
+    ? { ...session.user, id: Number.parseInt(session.user.id, 10) }
+    : undefined;
+
+  if (event.path.startsWith("/dashboard")) {
     if (!session) {
       await sendRedirect(event, "/", 302);
     }
