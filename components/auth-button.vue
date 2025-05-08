c6fa2aaa6fa65a93f6ae9e99ac70ac6ae74f30c5
Mihai <mihai.ppscu@gmail.com>
2025-05-08 10:52:01 +0300

    fix: theming (#74)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -20,14 +20,6 @@ const authStore = useAuthStore();
       </div>
       {{ authStore.user.name }}
     </div>
-    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
-      <li>
-        <NuxtLink to="/sign-out">
-          <Icon name="tabler:logout-2" size="24" />
-          Sign Out
-        </NuxtLink>
-      </li>
-    </ul>
   </div>
   <button
     v-else
