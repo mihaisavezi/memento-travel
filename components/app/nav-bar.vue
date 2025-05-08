db18d0463dfc2957283b948f194b2b2ba2ee2e82
Mihai Popescu <mihai.ppscu@gmail.com>
2025-05-08 11:49:44 +0300

    fix: theming

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,11 +1,20 @@
 <script lang="ts" setup>
 const authStore = useAuthStore();
+const colorMode = useColorMode();
+const isDark = computed(() =>
+  colorMode.value === "dark");
 </script>
 
 <template>
-  <div class="navbar bg-base-300 text-white">
+  <div
+    class="navbar"
+    :class="{
+      'bg-indigo-950/60': isDark,
+      'bg-sky-200/50': !isDark,
+    }"
+  >
     <div class="navbar-start">
-      <NuxtLink to="/" class="btn btn-ghost text-xl text-base-content">
+      <NuxtLink to="/" class="btn btn-ghost text-xl tracking-wide font-light text-base-content font-serif">
         Memento
       </NuxtLink>
     </div>
@@ -23,7 +32,7 @@ const authStore = useAuthStore();
           </div>
           {{ authStore.user.name }}
         </div>
-        <ul tabindex="0" class="dropdown-content menu bg-base-200 text-base-content rounded-box z-1 w-52 p-2 shadow-sm">
+        <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
           <li>
             <NuxtLink to="/sign-out">
               <Icon name="tabler:logout-2" size="24" />
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <div class="flex flex-1 bg-radial-[at_50%_75%] from-sky-300 via-blue-500 to-indigo-950 to-90%">
+  <div class="flex flex-1 bg-radial-[at_50%_75%] from-sky-300 via-base-500 to-indigo-950 to-90%">
     <div class="hero container mx-auto mt-4 flex flex-col">
-      <div class="flex flex-1 min-w-xl min-h-96">
+      <div class="flex flex-1 min-w-md max-h-96">
         <HeroMain />
       </div>
       <div class="hero-content text-center min-h-96 flex-col justify-start">
-        <div class="max-w-md">
-          <h1 class="text-5xl font-bold">
+        <div class="max-w-md text-base-100">
+          <h1 class="text-6xl font-serif">
             Memento
           </h1>
-          <p class="py-6">
-            Easily document your travels and adventures with this intuitive travel log app. Save locations,
+          <h3>
+            all your memories in one place
+          </h3>
+          <p class="py-12 text-sm text-base-200">
+            Save locations,
             upload photos, and write notes to build a vivid digital journal of your journeys.
           </p>
           <NuxtLink v-if="!authStore.user">
@@ -24,9 +27,14 @@ const authStore = useAuthStore();
               </button>
             </NuxtLink>
           </NuxtLink>
-          <NuxtLink v-else to="/dashboard">
-            Go to Dashboard
-          </NuxtLink>
+          <div v-else class="flex gap-2 mx-auto justify-center">
+            <NuxtLink class="btn btn-primary" to="/dashboard/add">
+              Add a new place
+            </NuxtLink>
+            <NuxtLink class="btn btn-info btn-link" to="/dashboard">
+              Check where you've been
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
